7d103d34553ef616bba007bedc637e6ecfb5fce8
andela-tadesanya <adeyemi.adesanya@andela.com>
2017-10-12 14:26:12 +0100

    setup API urls

--- blogger/urls.py
+++ blogger/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+from article import views
+
+router = routers.DefaultRouter()
+router.register(r'articles', views.ArticleViewSet)
+router.register(r'categories', views.CategoryViewSet)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
