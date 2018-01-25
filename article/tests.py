30934078e4064cee64f09d60e116502f69c61396
andela-tadesanya <adeyemi.adesanya@andela.com>
2018-01-25 12:39:15 +0100

    add tests for serializers

--- article/tests.py
+++ article/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from models import Article, Category
+from serializers import ArticleSerializer, CategorySerializer
 
 
-# Create your tests here.
+# Test models.
 class ArticleModelsTestCase(TestCase):
     def setUp(self):
         cat = Category.objects.create(name="fiction")
@@ -24,3 +25,45 @@ class ArticleModelsTestCase(TestCase):
     def test_category_is_created_properly(self):
         cat = Category.objects.all().first()
         self.assertEqual(cat.name, "fiction")
+
+
+# Test Serializers.
+class ArticleSerializersTestCase(TestCase):
+    def setUp(self):
+        self.cat = Category.objects.create(name="fiction")
+        self.Article.objects.create(
+            writer="tosin",
+            title="things fall apart",
+            content="A very, very, loooong story",
+            category=self.cat,
+            image="path/to/img.jpg")
+
+    def tearDown(self):
+        pass
+
+    def test_category_serializer(self):
+        data = {'name': 'food'}
+        category_serializer = CategorySerializer(data=data)
+        self.assertTrue(category_serializer.is_valid())
+        category = category_serializer.save()
+
+        self.assertEqual(category.name, data['name'])
+
+    def test_article_serializer(self):
+        data = {
+            'writer': 'tosin',
+            'title': 'how to cook',
+            'content': 'boil water',
+            'category': self.cat.id,
+            'image': 'url/to/image'
+        }
+
+        article_serializer = ArticleSerializer(data=data)
+        self.assertTrue(article_serializer.is_valid())
+        article = article_serializer.save()
+
+        self.assertEqual(article.writer, data['writer'])
+        self.assertEqual(article.title, data['title'])
+        self.assertEqual(article.content, data['content'])
+        self.assertEqual(article.category, data['category'])
+        self.assertEqual(article.image, data['image'])
