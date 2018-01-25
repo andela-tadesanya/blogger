74c8376348281089bb93677361f616560fdcde3a
andela-tadesanya <adeyemi.adesanya@andela.com>
2018-01-25 13:18:55 +0100

    add permissions

--- article/views.py
+++ article/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
 from models import Article, Category
 from rest_framework import viewsets
 from serializers import ArticleSerializer, CategorySerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows articles to be viewed or edited.
     """
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
@@ -16,6 +18,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows category to be viewed or edited.
     """
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
