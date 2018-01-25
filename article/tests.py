e1428dd7bc5819f59287ecae5906707eea94a325
andela-tadesanya <adeyemi.adesanya@andela.com>
2018-01-25 14:31:12 +0100

    add more tests

--- article/tests.py
+++ article/tests.py
@@ -23,6 +23,9 @@ class ArticleModelsTestCase(TestCase):
         post = Article.objects.all().first()
         self.assertEqual(post.writer, "tosin")
         self.assertEqual(post.title, "things fall apart")
+        self.assertEqual(post.content, "A very, very, loooong story")
+        self.assertEqual(post.category.name, "fiction")
+        self.assertEqual(post.image, "path/to/img.jpg")
 
     def test_category_is_created_properly(self):
         cat = Category.objects.all().first()
@@ -69,6 +72,7 @@ class ArticleSerializersTestCase(TestCase):
 class ArticleViewTestCase(TestCase):
     def setUp(self):
         self.cat = Category.objects.create(name="fiction")
+        Category.objects.create(name="programming")
         Article.objects.create(
             writer="tosin",
             title="things fall apart",
@@ -116,6 +120,33 @@ class ArticleViewTestCase(TestCase):
         self.assertEqual(response.status_code, 201)
         client.logout()
 
+    def test_user_can_update_article_when_logged_in(self):
+        client = APIClient()
+        client.login(username='admin', password='pass')
+        payload = {
+            'writer': 'john doe',
+            'title': 'how to cook',
+            'content': 'boil water',
+            'category': self.cat.id,
+            'image': 'url/to/image'
+        }
+        article_id = Article.objects.all().first().id
+        url = '/articles/{}/'.format(article_id)
+        response = client.put(url, payload, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['writer'], payload['writer'])
+        client.logout()
+
+    def test_user_can_delete_article_when_logged_in(self):
+        client = APIClient()
+        client.login(username='admin', password='pass')
+
+        article_id = Article.objects.all().first().id
+        url = '/articles/{}/'.format(article_id)
+        response = client.delete(url)
+        self.assertEqual(response.status_code, 204)
+        client.logout()
+
     def test_user_can_view_category_without_logging_in(self):
         client = APIClient()
         response = client.get('/categories/')
@@ -137,4 +168,27 @@ class ArticleViewTestCase(TestCase):
         }
         response = client.post('/categories/', payload, format='json')
         self.assertEqual(response.status_code, 201)
+        client.logout()
+
+    def test_user_can_update_category_when_logged_in(self):
+        client = APIClient()
+        client.login(username='admin', password='pass')
+        payload = {
+            'name': 'non-fiction'
+        }
+        cat = Category.objects.all().first()
+        url = '/categories/{}/'.format(cat.id)
+        response = client.put(url, payload, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['name'], 'non-fiction')
+        client.logout()
+
+    def test_user_can_delete_category_when_logged_in(self):
+        client = APIClient()
+        client.login(username='admin', password='pass')
+
+        cat = Category.objects.all().first()
+        url = '/categories/{}/'.format(cat.id)
+        response = client.delete(url)
+        self.assertEqual(response.status_code, 204)
         client.logout()
